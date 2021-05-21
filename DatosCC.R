aa993ce9d7135830594236cbfb27864dd680739a
kamecon <kamalspain49@gmail.com>
2021-05-21 22:04:27 +0200

    add CA data to TallerBrecha

---
+++ DatosCC.R
@@ -0,0 +1,66 @@
+
+# Cargar librerias --------------------------------------------------------
+
+pacman::p_load(IMFData, tidyverse)
+
+
+# Buscar codigos ----------------------------------------------------------
+
+
+availableDB <- DataflowMethod()
+
+availableDB %>% 
+  dplyr::filter(str_detect(DatabaseID, "^BOP"))
+
+BOP.available.codes <- DataStructureMethod("BOP")
+
+CodeSearch(BOP.available.codes, "CL_INDICATOR_BOP", "CA") %>% 
+  head(10)
+
+
+# Descarga de datos -------------------------------------------------------
+
+databaseID <- "BOP"
+startdate = "2001-01-01"
+enddate = "2020-12-31"
+checkquery = FALSE
+
+queryfilter_dc <- list(CL_FREQ = "A",
+                       CL_Country_PCTOT = c("AR","AT","AU","BE","BR","MX",
+                                            "CN","DK","FR","IN","IT","JP",
+                                            "DE","IE","NL","PT","CH","TR",
+                                            "GB","US","UY","SE","PE","SA"),
+                       CL_INDICATOR_BOP= "BCA_BP6_USD")
+
+CAquery_dc <- CompactDataMethod(databaseID, queryfilter_dc, startdate, enddate, 
+                                checkquery)
+
+
+# Convertir lista en data frame -------------------------------------------
+
+#Creamos un vector con el nombre de los paises para utilizarlo para nombrar los
+#data frames de la lista
+
+paises <- CAquery_dc$`@REF_AREA` %>% as.vector()
+
+names(CAquery_dc$Obs) <- paises
+
+paises2 <- BOP.available.codes$CL_AREA_BOP %>%
+  dplyr::filter(CodeValue %in% paises)
+
+#Creamos el data framne
+
+data_ca <- Map(cbind,  CAquery_dc$Obs,
+               country = CAquery_dc$`@REF_AREA` %>% as.list()
+              ) %>% #En cada df de la lista le añadimos una columna con el pais
+  bind_rows() %>%
+  as_tibble() %>% 
+  select(-`@OFFICIAL_BPM`) %>% 
+  rename(time=`@TIME_PERIOD`, value=`@OBS_VALUE` ) %>%
+  mutate(value=as.numeric(value)) %>%
+  mutate(time=as.numeric(time))
+
+
+# Guaradar los datos ------------------------------------------------------
+
+save(data_ca, file = "data_ca.RData")
